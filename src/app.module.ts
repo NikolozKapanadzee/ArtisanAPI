6d237dfd1555131b7e4aa59fc56c17136e62edf4
NikolozKapanadzee <nkapanadze369@gmail.com>
2025-12-18 17:50:49 +0400

    feat: created auth collection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ArtisanModule } from './artisan/artisan.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ArtisanModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL!),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
