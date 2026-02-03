269d72325d1c4924b6abc87cbb75b9973b3a53f3
NikolozKapanadzee <nkapanadze369@gmail.com>
2026-02-03 22:21:50 +0400

    feat: setuped swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,18 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('ArtisanAPI')
+    .setDescription(
+      'ArtisanAPI provides endpoints to execute server tasks and commands safely. ' +
+        'Use it to run maintenance scripts, manage cron jobs, or trigger backend tasks via HTTP requests. ' +
+        ' Only allow trusted users and whitelist commands to prevent security risks.',
+    )
+    .setVersion('1.0')
+    .addTag('Artisan')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
